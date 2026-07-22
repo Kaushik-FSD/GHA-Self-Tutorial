9acf51085b6dee8e979dda33ff040e92e3e1e184
Kaushik-FSD <rockstarkaushik1000@gmail.com>
2026-07-22 22:50:12 +0530

    added setup before phase 3

---
+++ src/main/java/com/example/GhaApplication.java
@@ -0,0 +1,12 @@
+// src/main/java/com/example/GhaPhase1Application.java
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhaPhase1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(GhaPhase1Application.class, args);
+    }
+}
--- src/main/java/secondFile.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class secondFile {
-    public static void function2() {
-        System.out.println("Second file java!!!");
-    }
-}
